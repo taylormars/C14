a9be142cebf4d65687fbc360ff1ed938a48d46c1
unknown <>
2016-09-08 09:30:56 +0800

    [上机实践]使用Intent隐式启动 掌握使用Intent隐式启动的方法。 1.<intent-filter>元素作用与含义 2.<intent-filter>写法，尤其是action,category等元素写法 3.掌握隐式启动使用方法。 4.将代码上传到github上。 5.将遇到的问题、分析与理解、知识点小结等上传到gitbook上去。

---
+++ app/src/main/java/com/example/lyt/c14/New.java
@@ -0,0 +1,19 @@
+package com.example.lyt.c14;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by liyutong on 2016/9/8.
+ */
+public class New extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newl);
+
+    }
+}
